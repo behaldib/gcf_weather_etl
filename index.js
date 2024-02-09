21b344b5bbd346fb2b1b13dc3f067b86ee24942d
Divaye Behal <dbehal@iu.edu>
2024-02-09 21:30:55 +0000

    END OF V2 - LOGGING DATA TO THE CONSOLE

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
+const {Storage} = require('@google-cloud/storage');
+const csv = require('csv-parser');
+
+
+
+
+
+
 exports.readObservation = (file, context) => {
-    console.log(`  Event: ${context.eventId}`);
-    console.log(`  Event Type: ${context.eventType}`);
-    console.log(`  Bucket: ${file.bucket}`);
-    console.log(`  File: ${file.name}`);
+   // console.log(`  Event: ${context.eventId}`);
+   // console.log(`  Event Type: ${context.eventType}`);
+   // console.log(`  Bucket: ${file.bucket}`);
+   // console.log(`  File: ${file.name}`);
+
+
+   const gcs = new Storage();
+
+   const datafile = gcs.bucket(file.bucket).file(file.name);
+
+
+   datafile.createReadStream()
+   .on('error', () => {
+     // handle an error
+     console.error(error);
+   })
+   .pipe(csv())
+   .on('data',(row) => {
+    // log raw data
+    console.log(row)
+
+   })
+   .on('end',() => {
+    // handle end of csv
+    console.log('end!');
+   })
+
 }
